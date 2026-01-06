31982b7d79f38d73ea9c208fa4b6b88e4f2a8af3
Huzaifa206 <huzaifaasiddiqui@gmail.com>
2026-01-07 02:09:18 +0500

    .

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://physical-ai-robotics-book.example.com',
+  url: 'https://speckit-plus-hackathon-phi.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
